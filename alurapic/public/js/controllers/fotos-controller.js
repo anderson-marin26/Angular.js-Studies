b243bdfca5e18918a0fd9963ef01f4a29dcaf45a
Anderson Marin <dam.marinramos@gmail.com>
2016-03-27 21:08:29 -0300

    Starting with modules

---
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -0,0 +1,6 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+	$scope.foto = {
+		titulo : 'Raposa',
+		url : 'http://4.bp.blogspot.com/-q8UCzJ8HiiE/T6tP55eTVQI/AAAAAAAARK4/MDssoN4uGM4/s1600/raposa-angolana-2.jpg'
+	};
+});
