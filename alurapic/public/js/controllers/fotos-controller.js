5a6d8854a5ad6a6450a3c99bd94b0ac9b711ff80
Anderson Marin <dam.marinramos@gmail.com>
2016-04-01 21:13:09 -0300

    Communication with the back end of the application

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,6 +1,28 @@
-angular.module('alurapic').controller('FotosController', function($scope) {
-	$scope.foto = {
-		titulo : 'Raposa',
-		url : 'http://4.bp.blogspot.com/-q8UCzJ8HiiE/T6tP55eTVQI/AAAAAAAARK4/MDssoN4uGM4/s1600/raposa-angolana-2.jpg'
-	};
+angular.module('alurapic').controller('FotosController', function($scope, $http) 
+{
+	$scope.fotos = [];
+
+	//pegando dados do servidor	
+	$http.get('v1/fotos')
+	.success(function(fotos)
+	{
+		$scope.fotos = fotos;
+	})
+	.error(function(erro)
+	{
+		console.log(erro);
+	});
+
+	/*
+	//fazendo requisição pra API
+	var promise = $http.get('v1/fotos');
+	//como o $http nao sabe quanto tempo o servidor vai demorar pra responder, use a funcao a seguir pra ele deixar o resto do codigo ser executado em quanto ele trabalha
+	promise.then(function(retorno)
+	{
+		$scope.fotos = retorno.data;
+	}).catch(function(error)
+	{
+		console.log(error);
+	});
+	*/
 });
